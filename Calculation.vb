6e6f66d77b32f052fa175f70fc2010800e3855e4
joeherwig <joachim.herwig@gmx.net>
2019-07-11 23:57:37 +0200

    Added NAV 1 CDI and NAV 1 GSI for the updated version of portable-sim-panels repo.

--- Calculation.vb
+++ Calculation.vb
@@ -6,7 +6,8 @@ Partial Public Class MainWindow
                {"AIRSPEED_INDICATED", "/128"},
                {"PLANE_HEADING_DEGREES_MAGNETIC", "*360/65536/65536"},
                {"PLANE_BANK_DEGREES", "*(360/65536/65536)-360"},
-               {"PLANE_PITCH_DEGREES", "*(360/65536/65536)"}
+               {"PLANE_PITCH_DEGREES", "*(360/65536/65536)"},
+               {"VERTICAL_SPEED", "*60*3.28084/256 "}
            }
     Public Function calculateValue(ByVal key, ByVal rawValue)
         Dim temp As Decimal
--- FSUIPC.vb
+++ FSUIPC.vb
@@ -11,7 +11,11 @@ Partial Public Class MainWindow
         Public PLANE_HEADING_DEGREES_MAGNETIC As New Offset(Of UInteger)(&H580)
         Public PLANE_BANK_DEGREES As New Offset(Of UInteger)(&H57C)
         Public PLANE_PITCH_DEGREES As New Offset(Of UInteger)(&H578)
-
+        Public VERTICAL_SPEED As New Offset(Of Integer)(&H2C8)
+        Public NAV_1_CDI As New Offset(Of Single)(&H2AAC)
+        Public NAV_1_GSI As New Offset(Of Single)(&H2AB0)
+        Public NAV_2_CDI As New Offset(Of Single)(&H2AB4)
+        Public NAV_2_GSI As New Offset(Of Single)(&H2AB8)
     End Class
 
 End Class
