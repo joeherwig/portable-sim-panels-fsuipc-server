930df08efba27af25c28e6ca83546253bec2b1df
joeherwig <12555687+joeherwig@users.noreply.github.com>
2020-10-06 01:35:01 +0200

    added background-pic.

--- obj/Debug/portable-sim-panels-fsuipc-server_Content.g.i.cs
+++ obj/Debug/portable-sim-panels-fsuipc-server_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("favicon.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("airplane-airline-aircraft-travel.jpg")]
 
 
